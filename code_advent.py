d410a292359914d44e1a9fdeff3c7ed22b063254
khennessy <khennessy@demonware.net>
2018-12-05 20:55:19 +0000

    Work In Progress

--- code_advent.py
+++ code_advent.py
@@ -9,6 +9,10 @@ def print_debug(msg):
     if DEBUG == 1:
         print(msg)
 
+def print_good_debug(msg):
+    if DEBUG == 1:
+        print(msg)
+
 def read_file_into_list(filepath):
     # Read file into list of lines
     # For bigger files look into using yield and doing chunking, even just for practice
@@ -552,20 +556,48 @@ class Schedule:
             self.guard_dict[guard_string] = current_guard
             print_debug("current guard_dict is {}".format(self.guard_dict))
 
+        laziest_guard = None
         highest_minutes_asleep = 0
         for guard in self.guard_dict:
             if self.guard_dict[guard]["minutes_asleep_total"] > highest_minutes_asleep:
                 highest_minutes_asleep = self.guard_dict[guard]["minutes_asleep_total"]
                 laziest_guard = guard
 
-        print_debug("\nlaziest_guard is {} with a total of {} minutes\n".format(laziest_guard, highest_minutes_asleep))
-
+        self.laziest_guard = laziest_guard
+        self.highest_minutes_asleep = highest_minutes_asleep
 
 
     def get_answer_part_1(self):
 
+        print("")
+        print_debug("\nLaziest_guard is {} with a total of {} minutes\n".format(self.laziest_guard, self.highest_minutes_asleep))
 
-        return None
+
+        shifts_of_lazy_guard = [shift for shift in self.shift_list if shift['guard'] == self.laziest_guard]
+
+        print_debug("\nshifts_of_lazy_guard is {}".format(shifts_of_lazy_guard))
+        minute_frequencies = {}
+
+
+        for shift in shifts_of_lazy_guard:
+            for minute in range(60):
+                if shift['minutes_asleep'][minute] == '#':
+                    minute_frequencies[minute] = minute_frequencies.get(minute, 0)+1
+
+        print_debug("\nminute_frequencies for the lazy guard is {}".format(minute_frequencies))
+        print_debug("")
+
+        highest = [0, 0]
+        for minute in minute_frequencies:
+            if minute_frequencies[minute] > highest[1]:
+                highest[0] = minute
+                highest[1] = minute_frequencies[minute]
+
+        print_debug("favourite minute is {} with a frequency of {}\n\n\n".format(highest[0],highest[1]))
+
+        answer_string = "The laziest guard is {} with a total of {} minutes. " \
+                        "His favourite minute to sleep is {} with a frequency of {}".format(self.laziest_guard, self.highest_minutes_asleep, highest[0],highest[1])
+        return answer_string
 
     def build_dict(self, lines):
         for line in lines:
@@ -631,20 +663,20 @@ class Schedule:
 
         self.build_dict(lines)
 
-        self.build_guard_dict()
         self.highest_minutes_asleep = 0
         self.laziest_guard = None
 
         current_guard = None
+        self.build_guard_dict()
 
 
 
     def pretty_print(self):
         #print("\nshift_list = {}\n".format(self.shift_list))
 
-        print("Date   ID   Minute")
-        print("            000000000011111111112222222222333333333344444444445555555555")
-        print("            012345678901234567890123456789012345678901234567890123456789")
+        print_debug("Date   ID   Minute")
+        print_debug("            000000000011111111112222222222333333333344444444445555555555")
+        print_debug("            012345678901234567890123456789012345678901234567890123456789")
 
 
 
@@ -653,9 +685,34 @@ class Schedule:
             print_string = shift["date"]+"  "+shift["guard"].replace("Guard ","")+"  "
             for minute_char in shift["minutes_asleep"]:
                 print_string +=minute_char
-            print(print_string)
+            print_debug(print_string)
 
         #given a day, a guard ID and a minute
+        
+    def get_answer_part_2(self):
+
+        #dictionary with minute as keys to dict with guard ID as keys to frequency value
+        #
+
+        minute_freq_dict = {}
+        for x in range(60):
+            minute_freq_dict[x] = {}
+
+        for minute in range(60):
+            for shift in self.shift_list:
+                #if shift["minutes_asleep"] == '#':
+                guard_string = shift['guard']
+                new_dict = {guard_string: 0}
+                minute_freq_dict[minute].update(new_dict)
+
+                    #minute_freq_dict[minute] = minute_freq_dict.get(minute, [{"guard": None, "frequency": 0}])
+                    #pass
+
+           #minute_freq_dict[minute] = {"guard": None, "frequency": 0}
+        print_debug("minute_freq_dict is {}".format(minute_freq_dict))
+
+
+        return "WIP"
 
 def day_4_part1():
     lines = read_file_into_list("problem_4_dummy_input.txt")
@@ -671,7 +728,25 @@ def day_4_part1():
     my_schedule = Schedule(lines)
     my_schedule.pretty_print()
     answer = my_schedule.get_answer_part_1()
-    answer_string = "answer is {}".format(answer)
+    answer_string = "{}".format(answer)
+    return answer_string
+
+
+def day_4_part2():
+    lines = read_file_into_list("problem_4_dummy_input.txt")
+    #lines = read_file_into_list("problem_4_input.txt")
+
+    lines.sort()
+    for line in lines:
+
+        date_string = line.split(']')[0]
+        #print(date_string)
+        #print(line)
+        pass
+    my_schedule = Schedule(lines)
+    my_schedule.pretty_print()
+    answer = my_schedule.get_answer_part_2()
+    answer_string = "{}".format(answer)
     return answer_string
 
 def main():
@@ -703,6 +778,8 @@ def main():
     print("Answer for Day 3 - Part 2 - 'No Matter How You Slice It':\n------------------------\n" + str(day_3_part_2()))
     print("\n")
     print("Answer for Day 4 - Part 1 - 'Repose Record':\n------------------------\n" + str(day_4_part1()))
+    print("\n")
+    print("Answer for Day 4 - Part 2 - 'Repose Record':\n------------------------\n" + str(day_4_part2()))
     print("\n")
     print("Script end time is {}".format(str(datetime.datetime.now())))
 
